7e312e8fde8854f26e705839ffe06b761eb1d90b
mimitom10 <mimitom14@abv.bg>
2020-04-04 23:06:28 +0300

    Homework Upload

---
+++ CodeFirst/Data/HospitalContext.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.EntityFrameworkCore;
+using P01_HospitalDatabase.Data.Models;
+
+namespace P01_HospitalDatabase.Data
+{
+    public class HospitalContext : DbContext
+    {
+        public DbSet<Patient> Patients { get; set; }
+
+        public DbSet<Visitation> Visitations { get; set; }
+
+        public DbSet<Diagnose> Diagnoses { get; set; }
+
+        public DbSet<Medicament> Medicaments { get; set; }
+
+        public DbSet<PatientMedicament> PatientMedicaments { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder
+                    .UseSqlServer(DataSettings.ConnectionString);
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .Entity<Visitation>()
+                .HasOne(v => v.Patient)
+                .WithMany(p => p.Visitations)
+                .HasForeignKey(v => v.PatientId);
+
+            //modelBuilder
+            //    .Entity<Patient>()
+            //    .HasMany(p => p.Visitations)
+            //    .WithOne(v => v.Patient)
+            //    .HasForeignKey(v => v.PatientId);
+
+            modelBuilder
+                .Entity<Diagnose>()
+                .HasOne(d => d.Patient)
+                .WithMany(p => p.Diagnoses)
+                .HasForeignKey(d => d.PatientId);
+
+            modelBuilder
+                .Entity<PatientMedicament>()
+                .HasKey(pm => new
+                {
+                    pm.PatientId,
+                    pm.MedicamentId
+                });
+
+            modelBuilder
+                .Entity<PatientMedicament>()
+               .HasOne(pm => pm.Patient)
+               .WithMany(p => p.Prescriptions)
+               .HasForeignKey(pm => pm.PatientId);
+
+            modelBuilder
+                .Entity<PatientMedicament>()
+                .HasOne(pm => pm.Medicament)
+                .WithMany(m => m.Prescriptions)
+                .HasForeignKey(pm => pm.MedicamentId);
+        }
+    }
+}
---
+++ CodeFirst/StartUp.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using P01_HospitalDatabase.Data;
+
+
+namespace P01_HospitalDatabase
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+             var db = new HospitalContext();
+           // db.Database.Migrate();
+        }
+
+    }
+}
